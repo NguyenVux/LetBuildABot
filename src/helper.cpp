23ce0d02e26c6c66208093f733cb4e147ff24567
VuxNguyen <vu.ngguyenhoang@gmail.com>
2026-07-23 00:06:16 +0700

    add UIManager

--- src/helper.cpp
+++ src/helper.cpp
@@ -1,4 +1,7 @@
-
+#include "Player.h"
+#include "Instruction/Instruction.h"
+#include "Runner.h"
+#include <format>
 
 void DrawInfiniteGrid(const Camera2D& camera, int screenWidth, int screenHeight)
 {
@@ -29,5 +32,91 @@ void DrawInfiniteGrid(const Camera2D& camera, int screenWidth, int screenHeight)
         const float thickness = (y == 0) ? axisThickness : 1.0f;
         DrawLineEx({topLeft.x - s_tileSize, worldY}, {bottomRight.x + s_tileSize, worldY},
                    thickness, color);
+    }
+}
+
+void RunPlayerInstructionTest(int screenWidth, int screenHeight)
+{
+    Player player("assets/arrow.png");
+    State state = {
+        player,
+    };
+    MoveInstruction move;
+    MoveInstruction move2;
+    MoveInstruction move3;
+    RotateInstruction rot(90.0f);
+    DelayInstruction delay(0.5f);
+    DelayInstruction delay2(0.5f);
+    DelayInstruction delay3(0.5f);
+    DelayInstruction delay4(0.5f);
+    RotateInstruction rot2(-90.0f);
+
+    move.SetNext(&delay);
+    delay.SetNext(&rot);
+    rot.SetNext(&delay2);
+    delay2.SetNext(&move2);
+    move2.SetNext(&delay3);
+    delay3.SetNext(&rot2);
+    rot2.SetNext(&delay4);
+    delay4.SetNext(&move3);
+    move3.SetNext(nullptr);
+
+    Camera2D camera = {};
+    camera.offset = {screenWidth * 0.5f, screenHeight * 0.5f};
+    camera.target = player.position;
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    Instruction* instruction = &move;
+    instruction->Setup(state);
+
+    while (!WindowShouldClose())
+    {
+        camera.target = player.position;
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        if (instruction != nullptr)
+        {
+            if (!instruction->isDone(state))
+            {
+                instruction->Execute(state);
+            }
+            else
+            {
+                instruction = instruction->Next();
+                if (instruction != nullptr)
+                {
+                    instruction->Setup(state);
+                }
+            }
+        }
+
+        BeginMode2D(camera);
+        DrawInfiniteGrid(camera, screenWidth, screenHeight);
+        player.Draw();
+        EndMode2D();
+
+        if (instruction == nullptr)
+        {
+            DrawText("Done", 0, 0, 24, RAYWHITE);
+        }
+        else
+        {
+            MoveInstruction* moveInstruction = dynamic_cast<MoveInstruction*>(instruction);
+            if (moveInstruction != nullptr)
+            {
+                DrawText(std::format("Current Position: {} \n"
+                                     "Target Position: {} \n"
+                                     "Distance: \n {}",
+                                     player.position.x, moveInstruction->m_end.x,
+                                     Vector2Distance(moveInstruction->m_end, player.position))
+                             .c_str(),
+                         0, 0, 24, RAYWHITE);
+            }
+        }
+
+        EndDrawing();
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,162 +3,140 @@
 #include "Runner.h"
 #include "raylib.h"
 #include "raymath.h"
+#include <cassert>
+#include <cstddef>
 #include <format>
+#include <memory>
+#include <queue>
 #include <vector>
 
 void DrawInfiniteGrid(const Camera2D& camera, int screenWidth, int screenHeight);
 
-
-class Widget {
+class Widget
+{
 public:
-    std::vector<std::unique_ptr<Widget>> m_children;
+    std::string name;
+    std::vector<Widget*> m_children;
+    bool Visible = true;
+    bool Hover = false;
+    Rectangle rect;
+    virtual void UpdateLayout() {};
+    virtual void Draw() const = 0;
+    virtual ~Widget()
+    {
+    }
 };
 
-class Block
+class VContainerWidget : public Widget
 {
     public:
-    Rectangle rect;
-    void Update() const
+    float spacing = 5.0f;
+    float border = 5.0f;
+
+    virtual void UpdateLayout() override
     {
-        Vector2 mousePos = GetMousePosition();
-        bool hover = CheckCollisionPointRec(mousePos, rect);
-        Color color = GREEN;
-        if (hover)
+        rect.height = border * 2.0f;
+        int realChildCount = 0;
+        for (int i = 0; i < m_children.size(); i++)
         {
-            color = RED;
+            Widget* child = m_children[i];
+            if (child == nullptr)
+            {
+                TraceLog(LOG_WARNING, "%s::UpdateLayout() child index %d null", name.c_str(), i);
+                continue;
+            }
+            realChildCount++;
+            child->rect.x = rect.x;
+            child->rect.y = rect.y + border;
+            if (i > 0)
+            {
+                Widget* prevChild = nullptr;
+                int prev = i;
+                do
+                {
+                    prevChild = m_children[--prev];
+                } while (prevChild == nullptr && prev > 0);
+                if (prevChild != nullptr)
+                {
+                    child->rect.y = prevChild->rect.y + prevChild->rect.height + spacing;
+                }
+            }
+            rect.height += child->rect.height;
         }
+        rect.height += (realChildCount - 1.0f) * spacing;
+    }
+
+    virtual void Draw() const override
+    {
+        Color color = {89, 89, 89, 255};
         DrawRectangleRec(rect, color);
     }
 };
-
-struct Thickness
+class UIManager
 {
-    float top;
-    float bottom;
-    float left;
-    float right;
-};
-class WidgetContainer
-{
-    public:
-    Thickness paddings;
-    Thickness margins;
-    std::vector<Block> m_buttons;
-    Rectangle rect;
+public:
+    std::unique_ptr<Widget> root = nullptr;
 
-    void load()
+    void Update()
     {
-        for (int i = 0; i < m_buttons.size(); i++)
+        std::queue<Widget*> widgetsToDraw;
+        widgetsToDraw.push(root.get());
+        while (!widgetsToDraw.empty())
         {
-            Block& block = m_buttons[i];
-            if (i == 0)
+            const Widget* top = widgetsToDraw.front();
+            widgetsToDraw.pop();
+            if (!top)
             {
-                block.rect.x = rect.x;
-                block.rect.y = rect.y;
+                continue;
             }
-            else
+            if (!top->Visible)
             {
-                Block& prevBlock = m_buttons[i - 1];
-                block.rect.x = rect.x;
-                block.rect.y = prevBlock.rect.y + prevBlock.rect.height + 5.0f;
+                continue;
+            }
+            top->Draw();
+            for (Widget* child : top->m_children)
+            {
+                widgetsToDraw.push(child);            
             }
         }
     }
-
     void Draw()
     {
-        for (const Block& block : m_buttons)
+        std::queue<Widget*> widgetsToDraw;
+        widgetsToDraw.push(root.get());
+        while (!widgetsToDraw.empty())
         {
-            block.Update();
+            const Widget* top = widgetsToDraw.front();
+            widgetsToDraw.pop();
+            if (!top)
+            {
+                continue;
+            }
+            if (!top->Visible)
+            {
+                continue;
+            }
+            top->Draw();
+            for (Widget* child : top->m_children)
+            {
+                widgetsToDraw.push(child);
+            }
         }
     }
 };
 
-void RunPlayerInstructionTest(int screenWidth, int screenHeight)
+class Block : public Widget
 {
-    Player player("assets/arrow.png");
-    State state = {
-        player,
-    };
-    MoveInstruction move;
-    MoveInstruction move2;
-    MoveInstruction move3;
-    RotateInstruction rot(90.0f);
-    DelayInstruction delay(0.5f);
-    DelayInstruction delay2(0.5f);
-    DelayInstruction delay3(0.5f);
-    DelayInstruction delay4(0.5f);
-    RotateInstruction rot2(-90.0f);
-
-    move.SetNext(&delay);
-    delay.SetNext(&rot);
-    rot.SetNext(&delay2);
-    delay2.SetNext(&move2);
-    move2.SetNext(&delay3);
-    delay3.SetNext(&rot2);
-    rot2.SetNext(&delay4);
-    delay4.SetNext(&move3);
-    move3.SetNext(nullptr);
-
-    Camera2D camera = {};
-    camera.offset = {screenWidth * 0.5f, screenHeight * 0.5f};
-    camera.target = player.position;
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
-
-    Instruction* instruction = &move;
-    instruction->Setup(state);
-
-    while (!WindowShouldClose())
+    public:
+    virtual void Draw() const override
     {
-        camera.target = player.position;
-
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        if (instruction != nullptr)
-        {
-            if (!instruction->isDone(state))
-            {
-                instruction->Execute(state);
-            }
-            else
-            {
-                instruction = instruction->Next();
-                if (instruction != nullptr)
-                {
-                    instruction->Setup(state);
-                }
-            }
-        }
-
-        BeginMode2D(camera);
-        DrawInfiniteGrid(camera, screenWidth, screenHeight);
-        player.Draw();
-        EndMode2D();
-
-        if (instruction == nullptr)
-        {
-            DrawText("Done", 0, 0, 24, RAYWHITE);
-        }
-        else
-        {
-            MoveInstruction* moveInstruction = dynamic_cast<MoveInstruction*>(instruction);
-            if (moveInstruction != nullptr)
-            {
-                DrawText(std::format("Current Position: {} \n"
-                                     "Target Position: {} \n"
-                                     "Distance: \n {}",
-                                     player.position.x, moveInstruction->m_end.x,
-                                     Vector2Distance(moveInstruction->m_end, player.position))
-                             .c_str(),
-                         0, 0, 24, RAYWHITE);
-            }
-        }
-
-        EndDrawing();
+        Vector2 mousePos = GetMousePosition();
+        // bool hover = CheckCollisionPointRec(mousePos, rect);
+        Color color = Hover?RED:GREEN;
+        // }
+        DrawRectangleRec(rect, color);
     }
-}
+};
 
 void DrawTest()
 {
@@ -168,31 +146,31 @@ void DrawTest()
     InitWindow(screenWidth, screenHeight, "LetBuildAbot");
     SetTargetFPS(60);
 
-    // Camera2D cam;
-    WidgetContainer cont;
-    cont.rect.x = 0;
-    cont.rect.y = 0;
-    for (int i = 0; i < 10; i++)
+    UIManager manager;
+    manager.root = std::make_unique<VContainerWidget>();
+    manager.root->rect.y = 20;
+    manager.root->name = "root_VContainerWidget";
+    std::vector<Block> blocks(15);
+    for (Block& block : blocks)
     {
-        Block block;
         block.rect.width = 80;
         block.rect.height = 30;
-        cont.m_buttons.push_back(block);
+        manager.root->m_children.push_back(&block);
     }
-    cont.rect.x = screenWidth - 80;
-    cont.load();
+    
+    manager.root->UpdateLayout();
+    manager.root->rect.width = 100;
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        cont.Draw();
+        manager.Draw();
         EndDrawing();
     }
 
     CloseWindow();
 }
-
 
 int main()
 {
